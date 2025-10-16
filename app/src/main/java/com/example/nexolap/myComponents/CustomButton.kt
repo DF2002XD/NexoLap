de8afcd8a2ae9fe5a6b1a7917d511d8b4771b9e4
DF2002XD <superyo123456789@gmail.com>
2025-10-16 18:53:47 +0200

    añadidos los componentes TarjetaVertical y TarjetaHorizontal con sus respectivas vistas previas

--- app/src/main/java/com/example/nexolap/myComponents/CustomButton.kt
+++ app/src/main/java/com/example/nexolap/myComponents/CustomButton.kt
@@ -8,38 +8,3 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nexolap.R
 
-@Composable
-fun TarjetaVertical () {
-    Column {
-        Image(
-            painter = androidx.compose.ui.res.painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Play Button"
-        )
-        Text(text = "")
-    }
-}
-
-@Composable
-fun TarjetaHorizontal () {
-    Row{
-        Image(
-            painter = androidx.compose.ui.res.painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Play Button"
-        )
-        Column {
-            Text(text = "")
-        }
-    }
-}
-
-@Preview
-@Composable
-fun TarjetaVerticalPreview() {
-    TarjetaVertical()
-}
-
-@Preview
-@Composable
-fun TarjetaHorizontalPreview() {
-    TarjetaHorizontal()
-}
