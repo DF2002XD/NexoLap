af6b19b5d150896d6d3bec118a79d34b217337cd
KennySR <bryqnzr@gmail.com>
2025-11-10 21:40:21 +0100

    Añadidos componentes para la página de detalles, perfil y refactorización del inicio de sesión/registro.
    Se ha creado la nueva DetallesPage junto con los componentes Detalles y TablaEspecificaciones para mostrar información detallada de los productos.
    
    Se ha implementado el componente Perfil para la PerfilPage, permitiendo la visualización y edición de datos de usuario, y se ha añadido el CampoContrasenha reutilizable.
    
    Se han refactorizado los componentes InicioSesion y Registro para mejorar su estructura y la gestión del estado, y se han actualizado las clases de datos (Ordenador, UsuarioData) para ser más robustas.

--- app/src/main/java/com/example/nexolap/Pages/PerfilPage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nexolap.Pages
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun PerfilPage(modifier: Modifier = Modifier){
-
-}
--- app/src/main/java/com/example/nexolap/myComponents/CustomBusqueda.kt
+++
@@ -1,8 +0,0 @@
-package com.example.nexolap.myComponents
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun buscador (){
-
-}
